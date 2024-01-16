e139f6aab178bb81242e1223933bd8bad92e3d90
Ani <desai.dhwani89@gmail.com>
2024-01-16 11:56:21 +0000

    added employee details in data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,3 +5,8 @@ INSERT INTO COMPANIES (company_name)
         ('Fairfield & Company'),
         ('Dewey, Screwem and Howe');
 
+INSERT INTO EMPLOYEES (first_name, last_name, email, company_id)
+    VALUES
+        ('Dan', 'Vega', 'danvega@gmail.com', SELECT company_id FROM COMPANIES WHERE company_name = 'IBM'),
+        ('Jen', 'Vega', 'jenvega@gmail.com', SELECT company_id FROM COMPANIES WHERE company_name = 'Barclays'),
+        ('Bella', 'Vega', 'bellavega@gmail.com', SELECT company_id FROM COMPANIES WHERE company_name = 'Fairfield & Company');
