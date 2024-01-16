cdc071a2b36241c0fd13612d83cb233d0568dc81
Ani <desai.dhwani89@gmail.com>
2024-01-16 13:51:36 +0000

    added rental and maintenance into schema.sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -10,9 +10,31 @@ CREATE TABLE CUSTOMER (
 
 DROP TABLE IF EXISTS CAR;
 CREATE TABLE CAR (
- car_id INT AUTO_INCREMENT  PRIMARY KEY,
- model VARCHAR(100) NOT NULL,
- make VARCHAR(100) NOT NULL,
- rate_per_day DECIMAL (5, 2) NOT NULL,
- model_year INT NOT NULL
+  car_id INT AUTO_INCREMENT  PRIMARY KEY,
+  model VARCHAR(100) NOT NULL,
+  make VARCHAR(100) NOT NULL,
+  rate_per_day DECIMAL (5, 2) NOT NULL,
+  model_year INT NOT NULL
+);
+
+DROP TABLE IF EXISTS RENTAL;
+CREATE TABLE RENTAL (
+  rental_id INT AUTO_INCREMENT  PRIMARY KEY,
+  start_date DATE NOT NULL,
+  end_date DATE NOT NULL,
+  daily_rate DECIMAL NOT NULL,
+  car_id INT,
+  customer_id INT,
+  FOREIGN KEY (car_id) references CAR(car_id),
+  FOREIGN KEY (customer_id) references CUSTOMER(customer_id)
+);
+
+DROP TABLE IF EXISTS MAINTENANCE;
+CREATE TABLE MAINTENANCE  (
+  maintenance_id INT AUTO_INCREMENT  PRIMARY KEY,
+  maintenance_type varchar(250),
+  description varchar(250),
+  maintenance_date DATE,
+  car_id INT,
+  FOREIGN KEY (car_id) references CAR(car_id)
 );
